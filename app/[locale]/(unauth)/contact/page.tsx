682ab92d2d32836f1e303e4b18ae4d108de86082
Ahmed adel <itsahmedad@gmail.com>
2024-11-30 21:38:00 +0200

    feat(add): add contact

--- app/[locale]/(unauth)/contact/page.tsx
+++ app/[locale]/(unauth)/contact/page.tsx
@@ -9,7 +9,7 @@ export default function ContactPage() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Hero Section */}
-      <div className="relative h-[500px] w-full">
+      <div className="relative h-[550px] w-full">
         <div className="absolute inset-0">
           <Image
             layout="fill"
@@ -19,17 +19,17 @@ export default function ContactPage() {
           />
         </div>
         <div className="relative h-full flex items-center px-6 lg:px-8">
-          <h1 className="text-4xl font-bold text-white">Contacts</h1>
+          <h1 className="text-4xl font-bold text-white">Contact-us</h1>
         </div>
       </div>
 
       {/* Main Content */}
-      <div className="flex-1 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="flex-1 py-44 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
             {/* Contact Form */}
-            <div className="bg-white p-6 rounded-lg">
-              <h2 className="text-3xl font-bold text-gray-900 mb-8">
+            <div className=" p-6 rounded-lg">
+              <h2 className="text-6xl font-bold text-darkgreen mb-8">
                 Bring nature<br />in your home
               </h2>
               <form className="space-y-6">
@@ -81,7 +81,7 @@ export default function ContactPage() {
             </div>
 
             {/* Map */}
-            <div className="h-[600px] lg:h-auto relative">
+            <div className="h-[500px] lg:h-auto relative">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.9916256937595!2d2.2922926!3d48.8583736!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zM8KwNTEnMzAuMCJOIDLCsDE3JzMyLjQiRQ!5e0!3m2!1sen!2s!4v1629789309"
                 width="100%"
